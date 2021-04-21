387f3d575b863ad3ac1d99b8d61102016aa38edc
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-04-21 19:23:28 -0400

    aora si

--- src/commands/developer/eval.ts
+++ src/commands/developer/eval.ts
@@ -2,6 +2,7 @@ import Command from '../../Utils/Classes/command';
 import commandinterface from '../../Utils/Interfaces/run'
 import { Util, MessageEmbed } from 'discord.js-light'
 import replace from '../../Utils/Functions/replace'
+import { inspect } from 'util'
 
 class Comando extends Command {
 
@@ -19,7 +20,7 @@ class Comando extends Command {
             let code = args.join(" ");
             let evalued = await eval(code);
             let TYPE = typeof (evalued)
-            evalued = (await import("util")).inspect(evalued, { depth: 0 });
+            evalued = inspect(evalued, { depth: 0 });
             evalued = replace(evalued, [process.env.PASSWORD, client.token, process.env.PASSWORDDBL, process.env.MONGODB, process.env.WEBHOOKID, process.env.WEBHOOKTOKEN, process.env.DBLTOKEN, require('mongoose').connection.pass, require('mongoose').connection.user, require('mongoose').connection.host])
             const res = Util.splitMessage(evalued, { char: '', maxLength: 2000 });
 
