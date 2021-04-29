73addc590a3a27229b87dd44b9bfcb9207cea9d6
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-04-29 13:09:38 -0400

    command avatar

---
+++ src/commands/util/avatar.ts
@@ -0,0 +1,20 @@
+import Command from '../../Utils/Classes/command.js'
+import run from '../../Utils/Interfaces/run.js';
+export default class Comando extends Command {
+    constructor() {
+        super()
+        this.name = "avatar"
+        this.category = 'utils'
+    }
+    run({ message }: run) {
+
+        const user = message.mentions.users.first() || message.author,
+            avatar = user.displayAvatarURL({
+                dynamic: true,
+                size: 2048
+            });
+
+        return message.channel.send(`> ${avatar}`)
+
+    }
+};
