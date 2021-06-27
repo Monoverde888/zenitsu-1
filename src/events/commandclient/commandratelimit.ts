abadb79e580409fc32b1733432c6ce5e7815df55
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2021-06-27 14:44:57 -0400

    Add files via upload

---
+++ src/events/commandclient/commandratelimit.ts
@@ -0,0 +1,12 @@
+import detritus from 'detritus-client';
+import CommandClientType from '../../utils/classes/commandclient.js';
+
+function commandRatelimit(_client: detritus.ShardClient, _commandClient: CommandClientType, data: detritus.Command.CommandEvents.CommandRatelimit) {
+  for (const i of data.ratelimits.filter(x => !x.item.replied && data.context.canReply)) {
+    if (i.remaining < 2000) continue;
+    i.item.replied = true;
+    return data.context.reply(`Try after ${i.remaining / 1000} seconds (${i.remaining}ms).`);
+  };
+}
+
+export default commandRatelimit;
