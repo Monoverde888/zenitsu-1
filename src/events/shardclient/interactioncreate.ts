2ff7281f496b5cafef5ea80c749490ab0d79eae4
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-06-27 15:07:04 -0400

    Asi es, soy un crackster

--- src/events/shardclient/interactioncreate.ts
+++ src/events/shardclient/interactioncreate.ts
@@ -2,9 +2,9 @@ import detritus from 'detritus-client';
 import CommandClientType from '../../utils/classes/commandclient.js';
 import Collector, { Fixed } from '../../utils/collectors/button.js';
 
-function iteractionCreate(_client: detritus.ShardClient, _commandClient: CommandClientType, data: detritus.GatewayClientEvents.InteractionCreate) {
+function interactionCreate(_client: detritus.ShardClient, _commandClient: CommandClientType, data: detritus.GatewayClientEvents.InteractionCreate) {
   if ((data.interaction.type == 3) && ((data.interaction as Fixed).data.customId))
     Collector.handleInteractionCreate(data.interaction as Fixed);
 }
 
-export default iteractionCreate;
+export default interactionCreate;
