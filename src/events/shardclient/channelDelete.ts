a5b1d564684791d618e1de7c93efaea82b14623d
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2021-06-27 14:51:44 -0400

    Delete channelDelete.ts

--- src/events/shardclient/channelDelete.ts
+++
@@ -1,9 +0,0 @@
-import detritus from 'detritus-client';
-import CommandClientType from '../../utils/classes/commandclient.js';
-import Collector from '../../utils/collectors/button.js';
-
-function channelDelete(_client: detritus.ShardClient, _commandClient: CommandClientType, data: detritus.GatewayClientEvents.ChannelDelete) {
-  Collector.handleChannelDelete(data);
-}
-
-export default channelDelete
