68b9c0403a53ca57ada3aae5991c02f24e7c3ff2
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2021-06-27 14:52:04 -0400

    Delete messageDelete.ts

--- src/events/shardclient/messageDelete.ts
+++
@@ -1,9 +0,0 @@
-import detritus from 'detritus-client';
-import CommandClientType from '../../utils/classes/commandclient.js';
-import Collector from '../../utils/collectors/button.js';
-
-function messageDelete(_client: detritus.ShardClient, _commandClient: CommandClientType, data: detritus.GatewayClientEvents.MessageDelete) {
-  Collector.handleMessageDelete(data);
-}
-
-export default messageDelete;
