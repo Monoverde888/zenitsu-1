b9c111452b5c23b7a85437c7b0d67d01e44c1fee
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2021-06-27 14:51:48 -0400

    Delete guildDelete.ts

--- src/events/shardclient/guildDelete.ts
+++
@@ -1,9 +0,0 @@
-import detritus from 'detritus-client';
-import CommandClientType from '../../utils/classes/commandclient.js';
-import Collector from '../../utils/collectors/button.js';
-
-function guildDelete(_client: detritus.ShardClient, _commandClient: CommandClientType, data: detritus.GatewayClientEvents.GuildDelete) {
-  Collector.handleGuildDelete(data);
-}
-
-export default guildDelete;
